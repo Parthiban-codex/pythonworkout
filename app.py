53cea79875f2b36e5d8b70b3c4555077b04fb80d
Parthiban-codex <parthibanzy.033@gmail.com>
2026-09-01 04:09:50 +0530

    test1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",              # Put your MySQL password here
+        password="",              
         database="college_db"
     )
 
