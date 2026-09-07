dd192eda7bd5e1f61982f19b9a0dff1669a0064f
Parthiban-codex <parthiban.zy033@gmail.com>
2026-09-07 21:41:57 +0530

    using *args

--- fun1.py
+++ fun1.py
@@ -1,3 +1,9 @@
 def add(a,b):
     return a+b
     
+
+def pro(*args):
+    val=0
+    for i in args:
+        val+=i
+    return val
--- fun2.py
+++ fun2.py
@@ -1,3 +1,6 @@
-from fun1 import add 
+from fun1 import add,pro
 result=(add(2,6))
 print(result) 
+
+v=pro(1,2,3,4)
+print(v)
