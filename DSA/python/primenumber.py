24287fb8a4cb5edf086462930a5c3ff7b489bde2
Parthiban-codex <parthiban.zy033@gmail.com>
2026-09-21 21:09:20 +0530

    find prime number

---
+++ DSA/python/primenumber.py
@@ -0,0 +1,16 @@
+class prime():
+    def prime_number(self):
+        number=int(input("give num: "))
+
+        if number<=1:
+            return "not a prime"
+        for i in range(2,number):
+            if number % i == 0:
+                return "not a prime"
+        return "prime number"   
+
+
+obj=prime()
+print(obj.prime_number())
+            
+           
