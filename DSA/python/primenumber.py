ceb85a5cec94e2365f03b2ce956da6615e4aadf5
Parthiban-codex <parthiban.zy033@gmail.com>
2026-09-23 12:34:17 +0530

    find prime number

--- DSA/python/primenumber.py
+++ DSA/python/primenumber.py
@@ -4,7 +4,7 @@ class prime():
 
         if number<=1:
             return "not a prime"
-        for i in range(2,number):
+        for i in range(2,number//2):
             if number % i == 0:
                 return "not a prime"
         return "prime number"   
