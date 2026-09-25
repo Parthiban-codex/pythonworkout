f2886167bb84aeb79fc43ab39832ccb5b6f86fd8
Parthiban-codex <parthiban.zy033@gmail.com>
2026-09-25 17:33:23 +0530

    text crunching

--- DSA/python/Reversenum.py
+++ DSA/python/Reversenum.py
@@ -1,7 +1,24 @@
 n=int(input())
 rev=0
-while n>0: #123>0 t
+while n>0: #123>0 T
     v=n%10 #get last digit by (%) #123 % 10 = 3 
     rev=rev*10+v #0 * 10 = 0 + 3
     n=n//10 #remove last digit by (//) # n=123//10 = 12
-print(rev)    
+print(rev)  
+
+#reverse in oop
+class reverse(): 
+    rev_digit=0  
+    def rev(self):
+        num=int(input())
+        
+        while num>0:
+            last_digit=n%10
+            rev_digit=rev_digit*10+last_digit
+            num=num//10
+        print(f"reversed digit: {rev_digit}") 
+obj=reverse()
+obj.rev() 
+
+
+        
